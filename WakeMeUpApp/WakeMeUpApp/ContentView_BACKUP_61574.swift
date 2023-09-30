a27607f40e7cc763bba09e71622c93b78bf39df0
Apollonoff <apollonoff.arseniy@gmail.com>
2023-09-30 15:59:12 +0300

    -

---
+++ WakeMeUpApp/WakeMeUpApp/ContentView_BACKUP_61574.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  WakeMeUpApp
+//
+//  Created by Arseniy Apollonov on 31.08.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+<<<<<<< .merge_file_arNIbI
+        TabBarView()
+=======
+       DataTest()
+>>>>>>> .merge_file_zFcRlV
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+        //            .preferredColorScheme(.dark)
+    }
+}
+
+
