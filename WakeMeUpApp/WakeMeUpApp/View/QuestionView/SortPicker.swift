b7bd17eb5e9ec9b2cac2a65e2a2e153cb6a15fa3
Apollonoff <apollonoff.arseniy@gmail.com>
2023-09-30 15:37:29 +0300

    Customize Picker in QuestionView

--- WakeMeUpApp/WakeMeUpApp/View/QuestionView/SortPicker.swift
+++ WakeMeUpApp/WakeMeUpApp/View/QuestionView/SortPicker.swift
@@ -21,11 +21,23 @@ struct SortPicker: View {
                 Text("Category").tag(0)
                 Text("Complexity").tag(1)
                 Text("Likes").tag(2)
+                
             }
             .pickerStyle(.segmented)
             .padding(.leading, 20)
             .padding(.trailing, 20)
         }
+        
+    }
+    init() {
+    // Sets the background color of the Picker
+       UISegmentedControl.appearance().backgroundColor = UIColor(cgColor: bgCellColor)
+    // Disappears the divider
+       UISegmentedControl.appearance().setDividerImage(UIImage(), forLeftSegmentState: .normal, rightSegmentState: .normal, barMetrics: .default)
+    // Changes the color for the selected item
+       UISegmentedControl.appearance().selectedSegmentTintColor = UIColor(cgColor: tabBarColor)
+//    // Changes the text color for the selected item
+        UISegmentedControl.appearance().setTitleTextAttributes([.foregroundColor: UIColor(cgColor: dividerColor)], for: .selected)
     }
 }
 
